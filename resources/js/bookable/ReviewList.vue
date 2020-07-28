1ca295687f61903e5b9a31b0c1597eebeda1da69
Jan Hazincak <jan.hazincak@gmail.com>
2020-07-28 02:53:29 +0100

    ReviewList component markup ()using responsive grid options

---
+++ resources/js/bookable/ReviewList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div style="padding: 1.25rem">
+        <h6 class="text-uppercase text-secondary font-weight-bolder pt-4">Review list</h6>
+        <div class="border-bottom d-none d-md-block">
+            <div class="row pt-3">
+                <div class="col-md-6">Jan Hazincak</div>
+                <div class="col-md-6 d-flex justify-content-end">STAR RATING</div>
+            </div>
+            <div class="row pt-3">
+                <div class="col-md-12">Added 5 minutes ago</div>
+            </div>
+            <div class="row pt-3">
+                <div class="col-md-12">Content of the review</div>
+            </div>
+        </div>
+    </div>
+</template>
