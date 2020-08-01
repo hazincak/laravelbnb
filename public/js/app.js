7e41e7046065bcd4c1177e1ccd4d953c635993c7
Jan Hazincak <jan.hazincak@gmail.com>
2020-08-01 18:56:20 +0100

    Reading Vuex state

--- public/js/app.js
+++ public/js/app.js
@@ -1971,8 +1971,9 @@ __webpack_require__.r(__webpack_exports__);
   },
   data: function data() {
     return {
-      from: null,
-      to: null,
+      // from: this.$store.state.lastSearch.from || null,
+      from: this.$store.state.lastSearch.from,
+      to: this.$store.state.lastSearch.to,
       loading: false,
       status: null
     };
