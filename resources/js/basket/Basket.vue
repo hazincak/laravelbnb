b68dc80a04c22edd3e0f8b9cee3ffbd0b5c139ee
Jan Hazincak <jan.hazincak@gmail.com>
2020-08-04 19:25:10 +0100

    Rendering purchase confirmation

--- resources/js/basket/Basket.vue
+++ resources/js/basket/Basket.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <div class="row">
+        <success v-if="success">Congratulations on your purchase!</success>
+        <div class="row" v-else>
             <div class="col-md-8" v-if="itemsInBasket">
                 <div class="row">
                     <div class="col-md-6 form-group">
@@ -158,6 +159,7 @@ export default {
     data(){
         return{
             loading: false,
+            bookingAttempted: false,
             customer:{
                 first_name: null,
                 last_name: null,
@@ -174,11 +176,15 @@ export default {
         ...mapGetters(["itemsInBasket"]),
         ...mapState({
             basket: state => state.basket.items
-        })
+        }),
+        success(){
+            return !this.loading && 0 === this.itemsInBasket && this.bookingAttempted;
+        }
     },
     methods: {
         async book (){
             this.loading = true;
+            this.bookingAttempted = false;
             this.errors = null;
 
             try {
@@ -195,6 +201,7 @@ export default {
                 this.errors = error.response && error.response.data.errors;
             }
             this.loading = false;
+            this.bookingAttempted = true;
         }
     }
 }
