db90680e33dd58ebb147aab6c4f242f920c0a0c0
Jan Hazincak <jan.hazincak@gmail.com>
2020-08-04 04:20:36 +0100

    Validating arrays of Input

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Bookable;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -16,14 +17,32 @@ class CheckoutController extends Controller
     public function __invoke(Request $request)
     {
         $data = $request->validate([
+            //Validating if it is an array
+            'bookings' => 'required|array|min:1',
+            //Validating arrays of input
+            'bookings.*.bookable_id' => 'required|exists:bookables,id',
+            'bookings.*.from' => 'required|date|after_or_equal:today',
+            'bookings.*.to' => 'required|date|after_or_equal:from',          
             'customer.first_name' => 'required|min:2',
             'customer.last_name' => 'required|min:2',
             'customer.street' => 'required|min:3',
             'customer.city' => 'required|min:2',
             'customer.email' => 'required|email',
-            'customer.country' => 'required|min:2',
+            'customer.county' => 'required|min:2',
             'customer.state' => 'required|min:2',
             'customer.zip' => 'required|min:2',
         ]);
+
+        //Validating if the booking is an object
+        $data = array_merge($data, $request->validate([
+            'bookings.*' => ['required', function($attribute, $value, $fail){
+                $bookable = Bookable::findOrFail($value['bookable_id']);
+
+                if(!$bookable->availableFor($value['from'], $value['to'])){
+                    $fail("The object is not available in given dates!");
+                }
+            }]
+        ]));
+        dd($data);
     }
 }
